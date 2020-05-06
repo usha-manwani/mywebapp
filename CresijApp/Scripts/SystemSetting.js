7f065f28f5d1f93611f51ba03d56ee91bb845641
Administrator <uphulwani1@gmail.com>
2020-05-06 17:13:44 +0800

    upload logo

---
+++ CresijApp/Scripts/SystemSetting.js
@@ -0,0 +1,43 @@
+﻿function SubmitInfo() {
+    var scname = document.getElementsByName("SchoolName");
+    var scengname = document.getElementsByName("SchoolEngName");
+    var logo = document.getElementsByName("SchoolLogo");
+}
+var dataString;
+$('#schoolLogo').on('change', function (e) {
+    var files = e.target.files;
+    
+    var myID = 3; //uncomment this to make sure the ajax URL works
+    if (files.length > 0) {
+        if (window.FormData !== undefined) {
+            reader.readAsArrayBuffer(files[0]);
+
+            $.ajax({
+                type: "POST",
+                url: '../Services/getOrganisationData.asmx/GetStudentDat?id=' + myID,
+                contentType: false,
+                processData: false,
+                data: data,
+                success: function (result) {
+                    console.log(result);
+                },
+                error: function (xhr, status, p3, p4) {
+                    var err = "Error " + " " + status + " " + p3 + " " + p4;
+                    if (xhr.responseText && xhr.responseText[0] == "{")
+                        err = JSON.parse(xhr.responseText).Message;
+                    console.log(err);
+                }
+            });
+        } else {
+            alert("This browser doesn't support HTML5 file uploads!");
+        }
+    }
+});
+
+var reader = new FileReader();
+
+reader.addEventListener("load", function () {
+    dataString = reader.result;
+    console.log("file read success");
+}, false);
+
---
+++ CresijApp/Services/SystemSetting.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace CresijApp.Services
+{
+    /// <summary>
+    /// Summary description for SystemSetting
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SystemSetting : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod(EnableSession = true)]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = false)]
+        public static string GenerateHtmlFromXml()
+        {
+            if (HttpContext.Current.Request.Files != null)
+            {
+                var file = HttpContext.Current.Request.Files[0];
+            }
+
+            return string.Empty;
+        }
+
+    }
+}
