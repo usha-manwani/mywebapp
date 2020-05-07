086b64c0ff13645d8ada3f36c3a891f9250ddd6f
Administrator <uphulwani1@gmail.com>
2020-05-07 17:47:04 +0800

    work on system settings page

--- CresijApp/Scripts/SystemSetting.js
+++ CresijApp/Scripts/SystemSetting.js
@@ -1,43 +1,98 @@
-﻿function SubmitInfo() {
-    var scname = document.getElementsByName("SchoolName");
-    var scengname = document.getElementsByName("SchoolEngName");
-    var logo = document.getElementsByName("SchoolLogo");
-}
-var dataString;
-$('#schoolLogo').on('change', function (e) {
-    var files = e.target.files;
-    
-    var myID = 3; //uncomment this to make sure the ajax URL works
-    if (files.length > 0) {
-        if (window.FormData !== undefined) {
-            reader.readAsArrayBuffer(files[0]);
+﻿$('#submitButton').off('click').on('click', function SubmitInfo() {
+    console.log("submit button clicked");
+    var scname = document.getElementsByName("SchoolName")[0].value;
+    var scengname = document.getElementsByName("SchoolEngName")[0].value;
+    var logo = $("#filepath").val();
+    //reserve info
+    if ($('input[name = "ReserveNonWorkDay"]').is(':checked'))
+        var reservenonworkday = $('input[name = "ReserveNonWorkDay"]').val();
+    else
+        var reservenonworkday = "No";
+    if ($('input[name = "ReserveAutoReview"]').is(':checked'))
+        var reserveautoreview = $('input[name = "ReserveAutoReview"]').val();
+    else
+        var reserveautoreview = "No";
+    var reservestartdate = $('input[name = "ReserveStartDate"]').val();
+    var reservestopdate = $('input[name = "ReserveStopDate"]').val();
+    //transfer info
+    if ($('input[name = "TransferNonWorkDay"]').is(':checked'))
+        var transfernonworkday = $('input[name = "TransferNonWorkDay"]').val();
+    else
+        var transfernonworkday = "No";
 
-            $.ajax({
-                type: "POST",
-                url: '../Services/getOrganisationData.asmx/GetStudentDat?id=' + myID,
-                contentType: false,
-                processData: false,
-                data: data,
-                success: function (result) {
-                    console.log(result);
-                },
-                error: function (xhr, status, p3, p4) {
-                    var err = "Error " + " " + status + " " + p3 + " " + p4;
-                    if (xhr.responseText && xhr.responseText[0] == "{")
-                        err = JSON.parse(xhr.responseText).Message;
-                    console.log(err);
-                }
-            });
-        } else {
-            alert("This browser doesn't support HTML5 file uploads!");
-        }
-    }
+    if ($('input[name = "TransferAutoReview"]').is(':checked'))
+        var transferautoreview = $('input[name = "TransferAutoReview"]').val();
+    else
+        var transferautoreview = "No";
+    var transferstartdate = $('input[name = "TransferStartDate"]').val();
+    var transferstopdate = $('input[name = "TransferStopDate"]').val();
+    
+    var sections = (function () {
+        var valor = [];
+        var section = [];
+        $('input[name = "Section"]').each(function () {
+            if (this.checked) {                
+                var starttime = $(this).children('input[name = "starttime"]').val();
+                var stoptime = $(this).children('input[name = "stoptime"]').val();
+                section.push($(this).val());
+                section.push(starttime);
+                section.push(stoptime);
+                valor.push(section);
+            }
+                
+        });
+        return valor;
+    })();
+
+    var days = (function () {
+
+        var day = [];
+        $('input[name = "weekday"]').each(function () {
+            if (this.checked) {
+                day.push($(this).val());
+            }
+        });
+        return day;
+    })();
+
+    var semstartdate = $('input[name = "semstartDate"]').val();
+    var totalweeks = $('input[name = "totalweek"]').val();
+    var totalweeks = $('input[name = "semester"]:checked').val();
 });
 
-var reader = new FileReader();
+$('#SchoolLogo').on('change', function () {
 
-reader.addEventListener("load", function () {
-    dataString = reader.result;
-    console.log("file read success");
-}, false);
+    $.ajax({
+        url: '../../../Services/Handler.ashx',
+        type: 'POST',
+        data: new FormData($('form')[0]),
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (file) {
+            $("#fileProgress").hide();
+            $("#lblMessage").html("<b>" + file.name + "</b> has been uploaded.");
+            $("#filepath").val(file.path);
+        },
+        error: function () {
+            $("#fileProgress").hide();
+            $("#lblMessage").html("<b>" + file.name + "</b> couldnt be uploaded.");
+        },
+        xhr: function () {
+            var fileXhr = $.ajaxSettings.xhr();
+            if (fileXhr.upload) {
+                $("progress").show();
+                fileXhr.upload.addEventListener("progress", function (e) {
+                    if (e.lengthComputable) {
+                        $("#fileProgress").attr({
+                            value: e.loaded,
+                            max: e.total
+                        });
+                    }
+                }, false);
+            }
+            return fileXhr;
+        }
+    });
+});
 
--- CresijApp/Services/SystemSetting.asmx.cs
+++ CresijApp/Services/SystemSetting.asmx.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
 using System.Web;
-using System.Web.Http;
-using System.Web.Http.Results;
+
 using System.Web.Script.Services;
 using System.Web.Services;
 
@@ -17,8 +16,8 @@ namespace CresijApp.Services
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
-    public class SystemSetting : System.Web.Services.WebService
+     [ScriptService]
+    public class SystemSetting : WebService
     {
 
         [WebMethod]
@@ -26,17 +25,14 @@ namespace CresijApp.Services
         {
             return "Hello World";
         }
-        [WebMethod(EnableSession = true)]
-        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = false)]
-        public static string GenerateHtmlFromXml()
+
+
+        [WebMethod]
+        public string Saveimage(string data)
         {
-            if (HttpContext.Current.Request.Files != null)
-            {
-                var file = HttpContext.Current.Request.Files[0];
-            }
-
-            return string.Empty;
+            byte[] imgarr = Convert.FromBase64String(data);
+            return "done";
+            /* Add further code here*/
         }
-
     }
 }
